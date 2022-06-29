21888f0087fe709e0740af32848b12aed61aea06
Narcoker <jykim9868@gmail.com>
2022-06-29 18:49:56 +0900

    Add: Nav-bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, Link } from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Intro from './pages/Intro'
+import Project from './pages/Project';
+import Blog from './pages/Blog';
+import Login from './pages/Login';
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Container-Navbar'>
+        <Navbar variant="light">
+          <Container fluid>
+            <Navbar.Brand href="#intro"><span className='Font-NotoSans white'>Narcoker_Frontend Developer</span></Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/"><span className='Font-NotoSans white'>Intro</span></Nav.Link>
+              <Nav.Link as={Link} to="/project"><span className='Font-NotoSans white'>Project</span></Nav.Link>
+              <Nav.Link as={Link} to="/blog"><span className='Font-NotoSans white'>Blog</span></Nav.Link>
+              <Nav.Link as={Link} to="/login"><span className='Font-NotoSans white'>Login</span></Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </div>
+
+      <div className='Container-Content'>
+        <Routes>
+          <Route exact path="/" element={<Intro></Intro>} />
+          <Route path="/project" element={<Project></Project>} />
+          <Route path="/blog" element={<Blog></Blog>} />
+          <Route path="/login" element={<Login></Login>} />
+          {/* <Route path='/:id'><p>정해진 경로 외의 페이지입니다.</p> </Route> */}
+
+        </Routes>
+      </div>
+
+      <div className='Container-footer'>
+
+      </div>
+
     </div>
   );
 }
