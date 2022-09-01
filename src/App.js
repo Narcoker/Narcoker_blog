85756b84969dff22d6abd91354b9c465c0524601
Narcoker <jykim9868@gmail.com>
2022-09-01 22:02:22 +0900

    Update: home url

--- src/App.js
+++ src/App.js
@@ -25,7 +25,8 @@ function App() {
                   <img className="logo" src={logo} alt="logo_white.png" />
                   &nbsp;Narcoker</span></Navbar.Brand>
                 <Nav className="me-auto">
-                  <Nav.Link as={NavLink} to="/"><span className="NavText">Home</span></Nav.Link>
+                  {/* <Nav.Link as={NavLink} to="/"><span className="NavText">Home</span></Nav.Link> */}
+                  <Nav.Link as={NavLink} to="/Narcoker_blog"><span className="NavText">Home</span></Nav.Link>
                   <Nav.Link as={NavLink} to="/project"><span className="NavText">Project</span></Nav.Link>
                   <span style={{ padding: "8px" }}><a href='https://velog.io/@narcoker' className="NavText" target='_blank' rel="noreferrer">Velog</a></span>
                   <span style={{ padding: "8px" }}><a href='https://github.com/Narcoker' className="NavText" target='_blank' rel="noreferrer">Github</a></span>
@@ -40,9 +41,10 @@ function App() {
 
       <div className="ContainerContent">
         <Routes>
-          <Route exact path="/" element={<Intro setNavbarState={setNavbarState} />} />
+          {/* <Route exact path="/" element={<Intro setNavbarState={setNavbarState} />} /> */}
+          <Route exact path="/Narcoker_blog" element={<Intro setNavbarState={setNavbarState} />} />
           <Route path="/project" element={<Project />} />
-          <Route path="/:id" element={<p>찾을 수 없는 페이지 입니다.</p>} />
+          {/* <Route path="/:id" element={<p>찾을 수 없는 페이지 입니다.</p>} /> */}
 
         </Routes>
       </div>
