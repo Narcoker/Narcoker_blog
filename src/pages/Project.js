630286dbb5d5cb81b3e4bed656f30902af569bd9
Narcoker <jykim9868@gmail.com>
2022-10-24 19:59:24 +0900

    API Test

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,13 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import logo from '../image/logo_black.png'
-import ProjectInit from './ProjectInit'
-import '../css/Project.css'
+import logo from '../image/logo_black.png';
+import ProjectInit from './ProjectInit';
+import '../css/Project.css';
+import { projects } from '../projectData/data';
+import axios from 'axios';
+
+
 function Project(props) {
     const [projectInitModalState, setProjectInitModalState] = useState(true);
-
+    const url = "../image/logo_black.png"
     useEffect(() => {
         props.setNavbarState(!projectInitModalState);
     }, [projectInitModalState, props])
+
+    console.log(projects[0].images[0]);
+
+    const getAPI = () => {
+        // fetch('https://d963-218-54-93-145.jp.ngrok.io/', { headers })
+        //   .then(res => res.json())
+        //   .then(res => {
+        //     console.log(res);
+        //   })
+        try {
+            const response = axios.get('https://d963-218-54-93-145.jp.ngrok.io/');
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     return (
         <div className='Project'>
@@ -19,6 +39,11 @@ function Project(props) {
 
             <img src={logo} width="200vw" alt='logo.png' ></img>
             <div>Project 페이지 입니다.</div>
+            <div>{projects[0].name}</div>
+            {/* <img src={projects[0].images[0]} alt="" width={"200px"}></img> */}
+            <button onClick={getAPI}>asdf</button>
+
+
         </div>
     );
 }
---
+++ src/projectData/data.js
@@ -0,0 +1,21 @@
+const projects = [
+    {
+        name: "Project1",
+        images: [require('../image/logo_black.png'), require('../image/profile.jpg')],
+        Developer: "김준서",
+        duration: "22.00.00~",
+        skills: "React",
+        description: "",
+    },
+
+    {
+        name: "Project2",
+        images: [require('../image/logo_black.png'), require('../image/profile.jpg')],
+        Developer: "김준서",
+        duration: "22.00.00~",
+        skills: "HTML",
+        description: "",
+    },
+
+];
+export { projects }
